3553f5f3ec83bff8bd3b112ef2a7cffd8a429796
Alexandra Tyurina <142999785+AlexandraYuT@users.noreply.github.com>
2024-11-05 16:20:17 +0400

    Entity.js

--- public/js/api/Entity.js
+++ public/js/api/Entity.js
@@ -3,22 +3,34 @@
  * Имеет свойство URL, равно пустой строке.
  * */
 class Entity {
+
+  static URL = '';
   /**
    * Запрашивает с сервера список данных.
    * Это могут быть счета или доходы/расходы
    * (в зависимости от того, что наследуется от Entity)
    * */
   static list(data, callback){
-
+    return createRequest({
+      url: this.URL,
+      method: 'GET',
+      data,
+      callback,
+    });
   }
-
+  
   /**
    * Создаёт счёт или доход/расход с помощью запроса
    * на сервер. (в зависимости от того,
    * что наследуется от Entity)
    * */
   static create(data, callback) {
-
+    return createRequest({
+    url: this.URL,
+    method: 'PUT',
+    data,
+    callback,
+  });
   }
 
   /**
@@ -26,6 +38,11 @@ class Entity {
    * (в зависимости от того, что наследуется от Entity)
    * */
   static remove(data, callback ) {
-
+    return createRequest({
+    url: this.URL,
+    method: 'DELETE',
+    data,
+    callback,
+  });
   }
 }
