125f6c2026dfb03ae44596fa438d88586720a8d2
Alexandra Tyurina <142999785+AlexandraYuT@users.noreply.github.com>
2024-11-05 16:21:50 +0400

    createRequest.js

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -2,6 +2,31 @@
  * Основная функция для совершения запросов
  * на сервер.
  * */
-const createRequest = (options = {}) => {
+const createRequest = (options) => {
+  let { url, method, callback } = options;
+  const xhr = new XMLHttpRequest();
+  const formData = new FormData();
+  xhr.responseType = "json";
 
+  if (method === "GET") {
+    url = url.concat("?", new URLSearchParams(options.data).toString());
+    xhr.open(method, url);
+  } else {
+    for (const key in options.data) {
+      formData.append(key, options.data[key]);
+      xhr.open(method, url);
+    }
+  }
+  try {
+    xhr.send(formData);
+  } catch (err) {
+    callback(err);
+  }
+  xhr.onload = () => {
+    callback(null, xhr.response);
+  };
+
+  xhr.onerror = () => {
+    callback(new Error("Ошибка!"));
+  };
 };
