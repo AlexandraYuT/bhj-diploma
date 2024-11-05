5a7160a5430e0cc27502c9126f94d8bf2d874297
Alexandra Tyurina <142999785+AlexandraYuT@users.noreply.github.com>
2024-11-05 16:40:57 +0400

    CreateTransactionForm.js

--- public/js/ui/forms/CreateTransactionForm.js
+++ public/js/ui/forms/CreateTransactionForm.js
@@ -3,29 +3,59 @@
  * создания новой транзакции
  * */
 class CreateTransactionForm extends AsyncForm {
-  /**
-   * Вызывает родительский конструктор и
-   * метод renderAccountsList
-   * */
-  constructor(element) {
-    super(element)
-  }
+    /**
+     * Вызывает родительский конструктор и
+     * метод renderAccountsList
+     * */
+    constructor(element) {
+        super(element);
+        this.modal = this.element.closest('.modal');
+        this.accountsList = this.element.querySelector('[name="account_id"]');
+        this.renderAccountsList();
+    }
 
-  /**
-   * Получает список счетов с помощью Account.list
-   * Обновляет в форме всплывающего окна выпадающий список
-   * */
-  renderAccountsList() {
+    /**
+     * Получает список счетов с помощью Account.list
+     * Обновляет в форме всплывающего окна выпадающий список
+     * */
+    renderAccountsList() {
+        if (!this.accountsList) {
+            return;
+        }
+        Account.list({}, (error, response) => {
+            if (error) {
+                throw new Error(error);
+            }
+            if (!response.success || response.data.length === 0) {
+                return;
+            }
+            this.accountsList.innerHTML = response.data.reduce((acc, item) => acc + this.getAccount(item), '');
+        });
+    }
 
-  }
+    getAccount(data) {
+        return `<option value="${data.id}">${data.name}</option>`;
+    }
 
-  /**
-   * Создаёт новую транзакцию (доход или расход)
-   * с помощью Transaction.create. По успешному результату
-   * вызывает App.update(), сбрасывает форму и закрывает окно,
-   * в котором находится форма
-   * */
-  onSubmit(data) {
 
-  }
+    /**
+     * Создаёт новую транзакцию (доход или расход)
+     * с помощью Transaction.create. По успешному результату
+     * вызывает App.update(), сбрасывает форму и закрывает окно,
+     * в котором находится форма
+     * */
+    onSubmit(data) {
+        Transaction.create(data, (error, response) => {
+            if (error) {
+                throw new Error(error);
+            }
+            if (!response.success) {
+                return;
+            }
+            this.element.reset();
+            App.getModal('newIncome').close();
+            App.getModal('newExpense').close();
+            App.update();
+        });
+    }
 }
