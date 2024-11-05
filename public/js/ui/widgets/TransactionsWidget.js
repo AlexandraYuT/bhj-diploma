eb06b979e7e771290952a1e19f17d725c7a340d6
Alexandra Tyurina <142999785+AlexandraYuT@users.noreply.github.com>
2024-11-05 16:38:37 +0400

    TransactionsWidget.js

--- public/js/ui/widgets/TransactionsWidget.js
+++ public/js/ui/widgets/TransactionsWidget.js
@@ -4,23 +4,35 @@
  * создания нового дохода или расхода
  * */
 
-class TransactionsWidget {
-  /**
-   * Устанавливает полученный элемент
-   * в свойство element.
-   * Если переданный элемент не существует,
-   * необходимо выкинуть ошибку.
-   * */
-  constructor( element ) {
+ class TransactionsWidget {
+   /**
+    * Устанавливает полученный элемент
+    * в свойство element.
+    * Если переданный элемент не существует,
+    * необходимо выкинуть ошибку.
+    * */
+   constructor(element) {
+     if (!element) {
+       throw new Error("Невалидное значение для TransactionsWidget");
+     }
 
+     this.element = element;
+     this.registerEvents();
+   }
+   /**
+    * Регистрирует обработчики нажатия на
+    * кнопки «Новый доход» и «Новый расход». 
+    * При нажатии вызывает Modal.open() для
+    * экземпляра окна
+    * */
+    registerEvents() {
+      const income = document.querySelector('.create-income-button');
+      income.addEventListener('click', () => {
+        App.getModal('newIncome').open();
+      });
+        const expense = document.querySelector('.create-expense-button');
+        expense.addEventListener('click', () => {
+        App.getModal('newExpense').open();
+    });
+    }
   }
-  /**
-   * Регистрирует обработчики нажатия на
-   * кнопки «Новый доход» и «Новый расход».
-   * При нажатии вызывает Modal.open() для
-   * экземпляра окна
-   * */
-  registerEvents() {
-
-  }
-}
