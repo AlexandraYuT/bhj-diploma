45a6e855eb7b48c8deaf36af485fba142280277f
Alexandra Tyurina <142999785+AlexandraYuT@users.noreply.github.com>
2024-11-05 16:36:53 +0400

    CreateAccountForm.js

--- public/js/ui/forms/CreateAccountForm.js
+++ public/js/ui/forms/CreateAccountForm.js
@@ -2,13 +2,23 @@
  * Класс CreateAccountForm управляет формой
  * создания нового счёта
  * */
-class CreateAccountForm extends AsyncForm {
-  /**
-   * Создаёт счёт с помощью Account.create и закрывает
-   * окно в случае успеха, а также вызывает App.update()
-   * и сбрасывает форму
-   * */
-  onSubmit(data) {
-
+ class CreateAccountForm extends AsyncForm {
+	       /**
+	       * Создаёт счёт с помощью Account.create и закрывает
+	       * окно в случае успеха, а также вызывает App.update()
+	       * и сбрасывает форму
+	       * */
+	      onSubmit(data) {
+	        Account.create(data, (error, response) => {
+		            if (error) {
+		              throw new Error(error);
+		            }
+	  	          if (!response.success) {
+		              return;
+		            }
+		            App.getModal('createAccount').close();
+		            this.element.reset();
+		            App.update()
+	        });
+	      }
   }
-}
